279f87081c3952c6c2a50f85c127631a24016171
chanferolino <chanferolino@gmail.com>
2016-02-23 19:24:18 +0800

    Implementation of Logics

--- app/src/main/java/main/com/ferolino/midtermp2/MainActivity.java
+++ app/src/main/java/main/com/ferolino/midtermp2/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
             super.onPreExecute();
 
 
-            mAdapter.addAll(bokses);
+//            mAdapter.addAll(bokses);
         }
     }
 }
